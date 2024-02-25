c7abbe4018b752f7700c770b709da50daec2c3a0
Vachell Christiansen Chai <vastiansen@gmail.com>
2024-02-25 12:38:24 +0700

    Add route for change name

--- routes/user_route.js
+++ routes/user_route.js
@@ -3,10 +3,13 @@ import express from 'express'
 import UserController from '../controllers/user_controller.js'
 
 import Validator from '../middlewares/validator.js'
+import Verificator from '../middlewares/verificator.js'
 
 const userRouter = express.Router()
 
 userRouter.post('/create', Validator.USER().create, UserController.POST().createUser)
 userRouter.post('/login', Validator.USER().login, UserController.POST().loginUser)
+
+userRouter.patch('/changeName', Verificator.SESSION().verification, Validator.USER().changeName, UserController.PATCH().changeName)
 
 export default userRouter
